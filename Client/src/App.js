2e810a2d5d2aa819dd47dbc40003569311ef4222
Kuldeep K Srivastava <kk23907.me@gmail.com>
2020-09-27 16:08:04 +0530

    Added ethereum contract and oracle

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,23 +6,67 @@ import Container from 'react-bootstrap/Container';
 import Burn from './Burn';
 import Mint from './Mint';
 import Accounts from "./Accounts";
+import getWeb3 from "./getWeb3";
+import txzWallet from "./txzWallet";
 
+const App = () => {
 
-function App() {
+  const [ethWallet, setEthWallet] = useState();
+  const [tezosWallet, setTxzWallet] = useState();
+  
+  useEffect(() => {
+    loadWallets();
+  });
+
+  const loadWallets = async() => {
+
+    try {
+      // console.log("api key" + process.env.REACT_APP_API_KEY);
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+  
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.web3.eth.getAccounts();
+      // const raghuAccounts = process.env.REACT_APP_ACCOUNT;
+      
+      console.log(accounts, "accounts");
+      setEthWallet(web3);
+
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      // console.error(error);
+    }
+
+    try {
+      const wallet = await txzWallet()
+      const tezos = wallet.toTezos();
+      const accountPkh = await tezos.wallet.pkh();
+      const accountBalance = await tezos.tz.getBalance(accountPkh);
+      console.info(`address: ${accountPkh}, balance: ${accountBalance}`);
+      setTxzWallet(tezos);
+    } catch (err) {
+       // Catch any errors for any of the above operations.
+       alert("Thanos Wallet not installed");
+      // console.error(error);
+    }
+
+  }
+
   return (
     <div className="App" style={{backgroundColor:'#fff1c1'}}>
         <div style={{height:20, backgroundColor:'#fe5f55'}}>
 
         </div>
-
-
         <Container style={{marginTop: 20}}>
             <Accounts/>
             <Row style={{marginTop: 20}}>
                 <Col sm={1} md={2} lg={3}></Col>
                 <Col>
                     <p>Mint bXTZ Tokens</p>
-                    <Mint/>
+                    <Mint ethWallet={ethWallet} txzWallet={tezosWallet}/>
                     <p style={{marginTop:100}}>Burn bXTZ Tokens</p>
                     <Burn/>
                 </Col>
--- Client/src/Mint.js
+++ Client/src/Mint.js
@@ -1,174 +1,141 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { ThanosWallet } from "@thanos-wallet/dapp";
-import getWeb3 from "./getWeb3";
+import ABI from './Contract/BiFrost';
 
+const Mint = ({ethWallet, txzWallet}) => {
 
-class Mint extends React.Component {
+    const [step, setStep] = useState(1);
+    const [whitelistedEthAddress, setWhiteListedEthAddress] = useState('');
+    const [depositAmount, setDepositAmount] = useState(0);
+    const [tezosAddress, setTezosAdress] = useState('Unlock Wallet');
+    const [ethereumAddress, setEthereumAddress] = useState('Unlock Wallet');
+    const [accounts, setAccounts] = useState([]);
+    const [web3, setWeb3] = useState();
+    const [tezosContract, setTezosContract] = useState();
+    const [ethereumContract, setEthereumContract] = useState()
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            step: 1,
-            whitelistedEthAddress: '',
-            depositAmount: 0,
-            tezosAddress:'Unlock Wallet',
-            ethereumAddress:'Unlock Wallet',
-            web3: null, 
-            accounts: null,
-    
-        }
-        this.loadWallet();
-    }
+    useEffect(() => {
+        setupAccounts();
+    },[ethWallet,txzWallet])
 
-    async loadWallet(){
+    const setupAccounts = async() => {
+        // ethereum address
+        if(ethWallet) {
+            const accounts = await ethWallet.web3.eth.getAccounts();
+            setAccounts(accounts);
+            setEthereumAddress(accounts[0]);
+            setWhiteListedEthAddress(accounts[0]);
 
-        try {
-            const available = await ThanosWallet.isAvailable();
-            if (!available) {
-            throw new Error("Thanos Wallet not installed");
-            }
-    
-            const wallet = new ThanosWallet("Bifrost Baker");
-            await wallet.connect("carthagenet");
-            const tezos = wallet.toTezos();
-    
-            const accountPkh = await tezos.wallet.pkh();
-            const accountBalance = await tezos.tz.getBalance(accountPkh);
+            // let provider = await ethWallet.infuraProvider();
+            // console.log(provider);
+            // setWeb3(provider);
 
-            this.setState({tezosAddress: accountPkh})
-    
-            console.info(`address: ${accountPkh}, balance: ${accountBalance}`);
-    
-            
-    
-        } catch (err) {
-            console.error(err);
-        }
-
-        try {
-            // Get network provider and web3 instance.
-            const web3 = await getWeb3();
-            const accounts = await web3.eth.getAccounts();
-            this.setState({ web3: web3, accounts: accounts, ethereumAddress: accounts[0] });
-          
-        } catch (error) {
-            // Catch any errors for any of the above operations.
-            alert(
-              `Failed to load web3, accounts, or contract. Check console for details.`,
-            );
-            console.error(error);
-          }
-
-
-
-
-
-
-    }
-
-    async callDepositEntryPoint(){
-
-        try {
-            const available = await ThanosWallet.isAvailable();
-            if (!available) {
-            throw new Error("Thanos Wallet not installed");
-            }
-    
-            const wallet = new ThanosWallet("Bifrost Baker");
-            await wallet.connect("carthagenet");
-            const tezos = wallet.toTezos();
-    
-            const accountPkh = await tezos.wallet.pkh();
-            const accountBalance = await tezos.tz.getBalance(accountPkh);
-    
-            console.info(`address: ${accountPkh}, balance: ${accountBalance}`);
-    
-            const bifrost = await tezos.wallet.at(
-                "KT1S94kmkfLx5whkDSk5QaJ539z32rBLqxra"
-                );   
-    
-            const operation = await bifrost.methods.deposit(this.state.whitelistedEthAddress).send({ amount: this.state.depositAmount });
-            await operation.confirmation();
-    
-        } catch (err) {
-            console.error(err);
-        }
-
-        this.setState({step: 2})
-    }
-
-    render(){
-        return(
-            <div style={box1}> 
-                <Form>
-                    <Form.Group controlId="formTezosAddress">
-                        <Form.Label>Tezos Deposit Address</Form.Label>
-                        <Form.Control  placeholder={this.state.tezosAddress} readOnly/>
-                    </Form.Group>
-
-                    <Form.Group controlId="formXTZAmount">
-                        <Form.Label>Amount of XTZ</Form.Label>
-                        <Form.Control  placeholder="0" onChange={(e)=> this.setState({depositAmount: e.target.value})}/>
-                        <Form.Text className="text-muted">
-                            Creates {this.state.depositAmount/1.5} bXTZ on Ethereum
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group controlId="formEthAddress">
-                        <Form.Label>Ethereum Address Receiving bXTZ</Form.Label>
-                        <Form.Control  placeholder={this.state.ethereumAddress} readOnly/>
-                    </Form.Group>
-                </Form>
-                {(this.state.step ===1) ?
-                    <div style={{width:'100%', marginTop:30}}>
-                        <div>
-                            <Button onClick={() => this.callDepositEntryPoint()} variant="info"  size="lg" style={{width:'50%'}}>
-                                Deposit XTZ
-                            </Button>
-                        </div>
-                        <div style={{marginTop: 20}}>
-                            <Button  variant="outline-light" size="lg" style={{width:'50%'}} disabled>
-                                Mint bXTZ
-                            </Button>
-                        </div>
-
-                    </div>
-                    :
-                    <div style={{width:'100%', marginTop:30}}>
-                        <div>
-                            <Button variant="outline-light" size="lg" style={{width:'50%'}} disabled>
-                                Deposit XTZ
-                            </Button>
-                        </div>
-                        <div style={{marginTop: 20}}>
-                            <Button  variant="info" size="lg" style={{width:'50%'}} >
-                                Mint bXTZ
-                            </Button>
-                        </div>
-
-                    </div>
+            console.log(ABI);
+            const instance = new ethWallet.web3.eth.Contract(
+				ABI,
+                "0x3A03B4EddF521a0Cb2bC761DD06A04F322eEe5F8",
+                {
+                    from:accounts[0]
                 }
-                
-            </div>
-        )
+            );
+
+            console.log(instance, "instance");
+            setEthereumContract(instance);
+        }
+       
+        if(txzWallet) {
+            // tezos address
+            const accountPkh = await txzWallet.wallet.pkh();
+            setTezosAdress(accountPkh);
+
+            console.log(accountPkh,"accountPkh");
+            const bifrost = await txzWallet.wallet.at(
+                "KT192uQ2Cjhr3Hgnf8438p3DxwhpdFrMTGyJ"
+                ); 
+            setTezosContract(bifrost);
+        }
     }
+
+    const callDepositEntryPoint = async() => {
+        try {  
+            const operation = await tezosContract.methods.deposit(whitelistedEthAddress.toLowerCase()).send({ amount: depositAmount });
+            await operation.confirmation();
+        } catch (err) {
+            console.error(err);
+        }
+        setStep(2);
+    }
+
+    const callMintTokens = async() => {
+        try {
+            const operation = await ethereumContract.methods.requestIfAddressWhitelisted().send({from:accounts[0]});
+            console.log(operation);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    const box1 = {
+        width:'100%',
+        height:500,
+        backgroundColor:'#FF6961',
+        borderRadius:50,
+        padding:30
+    }
+
+    return (
+        <div style={box1}> 
+        <Form>
+            <Form.Group controlId="formTezosAddress">
+                <Form.Label>Tezos Deposit Address</Form.Label>
+                <Form.Control  placeholder={tezosAddress} readOnly/>
+            </Form.Group>
+
+            <Form.Group controlId="formXTZAmount">
+                <Form.Label>Amount of XTZ</Form.Label>
+                <Form.Control  placeholder="0" onChange={(e)=> {setDepositAmount(e.target.value)}}/>
+                <Form.Text className="text-muted">
+                    Creates {depositAmount/1.5} bXTZ on Ethereum
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formEthAddress">
+                <Form.Label>Ethereum Address Receiving bXTZ</Form.Label>
+                <Form.Control  placeholder={ethereumAddress} readOnly/>
+            </Form.Group>
+        </Form>
+        {(step ===1) ?
+            <div style={{width:'100%', marginTop:30}}>
+                <div>
+                    <Button onClick={() => callDepositEntryPoint()} variant="info"  size="lg" style={{width:'50%'}}>
+                        Deposit XTZ
+                    </Button>
+                </div>
+                <div style={{marginTop: 20}}>
+                    <Button  variant="outline-light" size="lg" style={{width:'50%'}} disabled>
+                        Mint bXTZ
+                    </Button>
+                </div>
+
+            </div>
+            :
+            <div style={{width:'100%', marginTop:30}}>
+                <div>
+                    <Button variant="outline-light" size="lg" style={{width:'50%'}} disabled>
+                        Deposit XTZ
+                    </Button>
+                </div>
+                <div style={{marginTop: 20}}>
+                    <Button onClick={() => callMintTokens()} variant="info" size="lg" style={{width:'50%'}} >
+                        Mint bXTZ
+                    </Button>
+                </div>
+
+            </div>
+        }
+    </div>
+    );
 }
-
-const box1 = {
-    width:'100%',
-    height:500,
-    backgroundColor:'#FF6961',
-    borderRadius:50,
-    padding:30
-
-}
-
-//backgroundColor:'#FF6961',
-//backgroundColor:'#61F7FF',
-
-
-
 
 export default Mint;
